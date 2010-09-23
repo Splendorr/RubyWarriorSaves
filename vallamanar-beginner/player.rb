7222be49e958ff2c1886d23efa5b84836a2b6cb7
Splendorr <simolinic@gmail.com>
2010-09-23 16:10:32 -0400

    Extracted basic sequence into its own method that accepts a direction, and incorporated instructions to retreat if under fire and not directly engaged in combat. Should probably add instructions to retreat from melee range, also. Beat level 6, though.

--- vallamanar-beginner/player.rb
+++ vallamanar-beginner/player.rb
@@ -8,19 +8,13 @@ class Player
     end
   end
   
-  def play_turn(warrior)
-    @pc_name = "Vallamanar"
-    hearts = warrior.health
-    @hurts = taking_damage?(@health, hearts)
-    puts "At the beginning, hurts is #{@hurts}"    
-    puts "Behind #{@pc_name} is #{warrior.feel(:backward)}"
-    puts "Ahead of #{@pc_name} is #{warrior.feel}"
+  def standard_sequence_backup
     if warrior.feel.empty?
       if @hurts == false
-        if hearts <= 17
+        if @hearts <= 17
           puts "Vallamanar is hurt and must, regrettably, rest."
           warrior.rest!
-        elsif hearts >= 18
+        elsif @hearts >= 18
           puts "Vallamanar feels great, and proceeds!"
           warrior.walk!
         end
@@ -35,7 +29,58 @@ class Player
       puts "Vallamanar cries out, 'Die, foul beast!'"
       warrior.attack!
     end
-    #@hurts = taking_damage?(@health, hearts)
+  end
+  
+  def standard_sequence(d)
+    if @w.feel(d).empty?
+      if @hurts == false
+        if @hearts <= 17
+          puts "#{@pc_name} is hurt and apparently safe, and so must, regrettably, rest."
+          @w.rest!
+        elsif @hearts >= 18
+          puts "#{@pc_name} feels great, and proceeds!"
+          @w.walk!(d)
+        end
+      elsif @hurts == true
+        if @hearts <= 10
+          puts "#{@pc_name} is taking fire and near death, and must retreat!"
+          @w.walk!(:backward)
+        else
+          puts "#{@pc_name} is under siege, and must forge onward."
+          @w.walk!(d)
+        end
+      end
+    elsif @w.feel(d).captive?
+      puts "#{@pc_name} has located a Captive! Be free, Captive!"
+      @w.rescue!(d)
+    elsif @w.feel(d).wall?
+      @w.walk!
+    else
+      puts "#{@pc_name} cries out, 'Die, foul beast!'"
+      @w.attack!(d)
+    end
+  end
+  
+  def play_turn(warrior)
+    if @all_backed_up == nil
+      @all_backed_up = false
+    end
+    @w = warrior
+    @pc_name = "Vallamanar"
+    @hearts = warrior.health
+    @hurts = taking_damage?(@health, @hearts)
+    puts "At the beginning, hurts is #{@hurts}"    
+    puts "Behind #{@pc_name} is #{warrior.feel(:backward)}"
+    puts "Ahead of #{@pc_name} is #{warrior.feel}"
+    if @all_backed_up == false
+      standard_sequence(:backward)
+      if warrior.feel(:backward).to_s == "wall"
+        @all_backed_up = true
+      end
+    elsif @all_backed_up == true
+      standard_sequence(:forward)
+    end
+    #@hurts = taking_damage?(@health, @hearts)
     #puts "At the end, hurts is #{@hurts}"
     @health = warrior.health
   end
