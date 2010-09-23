fda5e2818178788a62ed834f7d0bbd94304b4099
Splendorr <simolinic@gmail.com>
2010-09-23 15:37:25 -0400

    Got to level 7, now I need to incorporate backwards movement.

--- vallamanar-beginner/player.rb
+++ vallamanar-beginner/player.rb
@@ -1,4 +1,5 @@
 class Player
+  
   def taking_damage?(prev, curr)
     if prev.to_i > curr.to_i
       return true
@@ -8,25 +9,34 @@ class Player
   end
   
   def play_turn(warrior)
-    hearts = warrior.health    
-    # warrior.feel
+    @pc_name = "Vallamanar"
+    hearts = warrior.health
+    @hurts = taking_damage?(@health, hearts)
+    puts "At the beginning, hurts is #{@hurts}"    
+    puts "Behind #{@pc_name} is #{warrior.feel(:backward)}"
+    puts "Ahead of #{@pc_name} is #{warrior.feel}"
     if warrior.feel.empty?
       if @hurts == false
         if hearts <= 17
+          puts "Vallamanar is hurt and must, regrettably, rest."
           warrior.rest!
-        else
+        elsif hearts >= 18
+          puts "Vallamanar feels great, and proceeds!"
           warrior.walk!
         end
-      else
+      elsif @hurts == true
+        puts "Vallamanar is under siege, and must forge onward."
         warrior.walk!
       end
     elsif warrior.feel.captive?
+      puts "Vallamanar has located a Captive! Be free, Captive!"
       warrior.rescue!
     else
+      puts "Vallamanar cries out, 'Die, foul beast!'"
       warrior.attack!
     end
-    @hurts = taking_damage?(@health, hearts)
-    puts "Hurts is #{@hurts}"
+    #@hurts = taking_damage?(@health, hearts)
+    #puts "At the end, hurts is #{@hurts}"
     @health = warrior.health
   end
   
