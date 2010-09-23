852aeb31d227d5b49aefe5d6256d4e04d2805c3c
Splendorr <simolinic@gmail.com>
2010-09-23 17:01:54 -0400

    Oh, yesssss. Conditional expression to check if there's a wizard visible but ensure that there's no captive. I don't have it checking which one comes FIRST in the order, but that should just require modifying the regexp. Got hung up because I had an elsif with no condition, rather than an else.

--- vallamanar-beginner/player.rb
+++ vallamanar-beginner/player.rb
@@ -38,8 +38,13 @@ class Player
           puts "#{@pc_name} is hurt and apparently safe, and so must, regrettably, rest."
           @w.rest!
         elsif @hearts >= 18
-          puts "#{@pc_name} feels great, and proceeds!"
-          @w.walk!(d)
+          if @targets_ahead.to_s =~ /Wizard/ && @targets_ahead.to_s !~ /Captive/
+            puts "#{@pc_name} sees a Wizard and has a clear shot."
+            @w.shoot!
+          else
+            puts "#{@pc_name} feels great, and proceeds!"
+            @w.walk!(d)
+          end
         end
       elsif @hurts == true
         if @hearts <= 10
@@ -54,7 +59,7 @@ class Player
       puts "#{@pc_name} has located a Captive! Be free, Captive!"
       @w.rescue!(d)
     elsif @w.feel(d).wall?
-      @w.walk!
+      @w.pivot!
     else
       puts "#{@pc_name} cries out, 'Die, foul beast!'"
       @w.attack!(d)
@@ -62,16 +67,18 @@ class Player
   end
   
   def play_turn(warrior)
-    if @all_backed_up == nil
-      @all_backed_up = false
-    end
+    @all_backed_up ||= false
+    @all_backed_up = true
     @w = warrior
     @pc_name = "Vallamanar"
     @hearts = warrior.health
     @hurts = taking_damage?(@health, @hearts)
+    @targets_ahead = warrior.look(:forward).to_a
+    @targets_behind = warrior.look(:backward).to_a
     puts "At the beginning, hurts is #{@hurts}"    
-    puts "Behind #{@pc_name} is #{warrior.feel(:backward)}"
-    puts "Ahead of #{@pc_name} is #{warrior.feel}"
+    puts "Behind #{@pc_name} is #{@targets_behind.join(", ")}."
+    puts "Ahead of #{@pc_name} is #{@targets_ahead.join(", ")}."
+
     if @all_backed_up == false
       standard_sequence(:backward)
       if warrior.feel(:backward).to_s == "wall"
